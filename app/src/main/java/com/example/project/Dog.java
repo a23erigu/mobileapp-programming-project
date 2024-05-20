ef4be4a27bcbf7dcf36daa3617710fe293e6b4dc
Pistolmannen <erikgustafsson03@gmail.com>
2024-05-20 11:31:22 +0200

    added atributs to dogs

--- app/src/main/java/com/example/project/Dog.java
+++ app/src/main/java/com/example/project/Dog.java
@@ -3,7 +3,11 @@ package com.example.project;
 public class Dog {
 
     String name;
+    String color;
+    String rasgrupp;
     int size;
+    int cutenes;
+    String picture;
 
     public Dog (String name){
         this.name = name;
@@ -12,14 +16,25 @@ public class Dog {
     public String getName() {
         return name;
     }
+    public String getColor() {
+        return color;
+    }
+    public String getRasgrupp() {
+        return rasgrupp;
+    }
     public int getSize() {
         return size;
+    }
+    public int getCutenes() {
+        return cutenes;
+    }
+    public String getPicture() {
+        return picture;
     }
 
     public void setName(String name) {
         this.name = name;
     }
-
     public void setSize(int size) {
         this.size = size;
     }
@@ -28,7 +43,11 @@ public class Dog {
     public String toString() {
         return "Dog{" +
                 "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", rasgrupp='" + rasgrupp + '\'' +
                 ", size=" + size +
+                ", cutenes=" + cutenes +
+                ", picture='" + picture + '\'' +
                 '}';
     }
 }
