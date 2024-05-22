018c9f34b8d5d57eacf67e5a6a009ef236d37949
Pistolmannen <erikgustafsson03@gmail.com>
2024-05-22 15:37:35 +0200

    added sort buttons

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23erigu";
 
     Button switchActivityButton;
+    Button sortAZButton;
+    Button sortZAButton;
 
     private RecyclerView recView;
     private RecyclerViewAdapter recViewAdapter;
